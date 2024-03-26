17c429f87c215de6820e1ba0cc8d15f3c01772de
The-Arun-singh <arsinghbusiness@gmail.com>
2024-03-26 23:25:04 +0530

    completed the Todo App's basic functionalities

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,5 +4,50 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  return <div>Add To Do</div>
+  const [input, setInput] = useState< string >('');
+
+  const { addTodo } = useTodo()
+
+  // const [todos, setTodos] = useState< string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [])
+
+  
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(input, input.trim())
+    if (input.trim() !== '') {
+      addTodo(input)
+      setInput("")
+      toast.success("Todos added successfully")
+    } else {
+      toast.error('Todo field cannot be empty')
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmission}>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input 
+          ref={inputRef}
+          type="text"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          className='w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zing-500 focus:border-white'
+          placeholder='Start typing...' 
+        />
+        <button
+          type='submit'
+          className='px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl'
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,39 @@
 import { InputHTMLAttributes, forwardRef } from 'react'
 import cn from 'classnames'
 
-import React from 'react'
+export const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({className, ...rest }, ref) => {
+  return (
+    <input 
+      {...rest}
+      ref={ref}
+      className={cn(
+        'w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white',
+        className,
+      )}
+    />
+  )
+})
 
-export const Input = () => {
-  return <div>Input</div>
-}
+// import React, { forwardRef, ForwardRefRenderFunction } from 'react';
+
+// // Define props types
+// interface MyComponentProps {
+//   // Define your component's props here
+//   text: string;
+//   onClick?: () => void;
+// }
+
+// // Define the component
+// const MyComponent: ForwardRefRenderFunction<HTMLDivElement, MyComponentProps> = ({ text, onClick }, ref) => {
+//   return (
+//     <div ref={ref} onClick={onClick}>
+//       {text}
+//     </div>
+//   );
+// };
+
+// // Forward the ref to the component
+// export const ForwardedMyComponent = forwardRef(MyComponent);
